146f693e7b94e6e70ce7790665e0edcf8b43de73
Murilo013 <muriloalessioamaral@gmail.com>
2025-10-20 18:25:40 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/projetos-feira-profissoes/",
+  base: "/projeto-feira-profissoes/",
   server: {
     host: "::",
     port: 8080,
