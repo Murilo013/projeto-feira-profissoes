36aba7dbf84c59efabbfee41afce5461d2ee56f7
Murilo013 <muriloalessioamaral@gmail.com>
2025-10-20 23:49:36 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/projeto-feira-profissoes",
+  base: process.env.VITE_BASE_PATH || "/projeto-feira-profissoes",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
